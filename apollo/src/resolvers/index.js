c7ec692880fa428884c5e5584195e1d2dde7a2ae
Aaron Clark <aaronpclark92@gmail.com>
2020-05-06 18:01:46 -0500

    feat: refactors general resolver for info query

--- apollo/src/resolvers/index.js
+++ apollo/src/resolvers/index.js
@@ -1,7 +1,8 @@
 // new approach: piece together each sub directory and export under resolvers
 import authResolvers from './auth'
 import userResolvers from './user'
+import generalResolvers from './general'
 
-const resolvers = [authResolvers, userResolvers]
+const resolvers = [authResolvers, userResolvers, generalResolvers]
 
 export default resolvers
