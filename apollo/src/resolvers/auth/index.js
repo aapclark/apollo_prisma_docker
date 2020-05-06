ae2a1273ae24bd0d6fe4806f1d0362a96b7fb9b6
Aaron Clark <aaronpclark92@gmail.com>
2020-05-06 17:09:12 -0500

    feat: refactors auth resolvers

---
+++ apollo/src/resolvers/auth/index.js
@@ -0,0 +1,8 @@
+import Mutation from './mutation'
+// import Query from './query'
+const authResolvers= {
+	Mutation,
+	// Query
+}
+
+export default authResolvers
---
+++ apollo/src/resolvers/auth/mutation.js
@@ -0,0 +1,50 @@
+import { bcrypt } from 'bcryptjs'
+import { generateToken, getUserId, hashPassword } from '../../auth'
+import { AuthenticationError, UserInputError } from 'apollo-server'
+
+const Mutation = {
+	register: async function (_, { input }, { prisma }) {
+	const { email, password } = input
+	const emailTaken = await prisma.$exists.user({
+		email
+		})
+
+	if (emailTaken) {
+		throw new UserInputError('Email already in use.')
+		}
+
+	else {
+		const hash = hashPassword(password)
+		const user = await prisma.createUser({
+			email,
+			password: hash
+			})
+		const token = generateToken(user)
+
+		return {
+			token,
+			user
+			}
+		}
+	}, 
+
+	login: async function (_, { input }, { prisma }) {
+		const { email, password } = input
+		const user = await prisma.user({
+			email
+		})
+		let passwordMatch
+		user && (passwordMatch = await bcrypt.compare(password, user.password))
+		if (!user || !passwordMatch) {
+			throw new AuthenticationError('Invalid Login.')
+		} else {
+			const token = generateToken(user)
+			return {
+				token,
+				user
+			}
+		}
+	}
+}
+
+export default Mutation
