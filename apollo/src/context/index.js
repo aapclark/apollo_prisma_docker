df8a52d03174a1db1a7ce1cc9fb012d8368b0a14
Aaron Clark <aaronpclark92@gmail.com>
2020-06-08 20:03:04 -0500

    feat: minor changes on Context

--- apollo/src/context/index.js
+++ apollo/src/context/index.js
@@ -2,9 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 // This Context creator assumes validation is handle within resolvers. Consult Apollo Documents for alternative options.
 const prisma = new PrismaClient()
-console.log(prisma)
 const createContext = async ({ req }) => {
-
   return { req, prisma }
 }
 
